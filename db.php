2ad84246e4e6723ebed5d9a12440c1f67b4bc58f
Wilmer Chala <wilmer.chala@cun.edu.co>
2025-03-11 22:07:06 -0500

    Actualizar configuración de conexión a la base de datos

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "Wilmer";
-$password = "123456";
+$servername = "mysql"; // Cambia "localhost" por "mysql" (nombre del servicio)
+$username = "Admin";
+$password = "Admin@2025";
 $dbname = "dependencias_db";
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
